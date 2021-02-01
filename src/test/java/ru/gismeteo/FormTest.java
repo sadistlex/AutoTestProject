24345eab6120924b58de42f856bc04e2164a49b0
sadistlex <78348729+sadistlex@users.noreply.github.com>
2021-02-01 15:19:01 +0300

    deleting unneeded imports

--- src/test/java/ru/gismeteo/FormTest.java
+++ src/test/java/ru/gismeteo/FormTest.java
@@ -5,9 +5,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.concurrent.TimeUnit;
 
 public class FormTest extends DriverSettings{
 
